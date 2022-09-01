13c506d657bb6f766e02a6507774288072f98924
Ashoka Pradeep <95160464+ashokapradeep@users.noreply.github.com>
2022-09-01 18:14:51 +0530

    responsive design

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let countAr = []
 
 function increment() {
     count += 1
@@ -13,8 +14,8 @@ function decrement() {
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+    countAr.push(count)
+    saveEl.textContent = "Previous entries:"+countAr
     countEl.textContent = 0
     count = 0
 }
